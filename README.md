3c80594796acb2453f2bb1f1985d434d86681381
D.Chinh <chinh6803@gmail.com>
2023-08-26 17:28:37 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Javascript
+# Python
---
+++ main.py
@@ -0,0 +1,26 @@
+# import math
+# # thu vien toan hoc
+# # method detal
+# def calculate_delta(number1, number2, number3):
+#     return number2 ** 2 - 4 * number1 * number3
+# #input number1, number2, number3
+# number1 = float(input("Enter number 1: "))
+# number2 = float(input("Enter number 2: "))
+# number3 = float(input("Enter number 3: "))
+# #conglution delta
+# delta = calculate_delta(number1, number2, number3)
+
+# if delta > 0:
+#     print("Equation has two solutions")
+#     x1 = (-number2 + math.sqrt(delta)) / (2 * number1)
+#     x2 = (-number2 - math.sqrt(delta)) / (2 * number1)
+#     print("x1 =", x1)
+#     print("x2 =", x2)
+# elif delta == 0:
+#     print("Equation has a single solution")
+#     x1 = - number2 / (2 * number1)
+#     print("x1 = x2", x1)
+# else:
+#     print("Equation has no real solution")
+
+
